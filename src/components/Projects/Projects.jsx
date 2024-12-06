e6ef089b463779d6f5ac5bea09c7bdfaecb3931f
Meratim <guerra.mercedes@hotmail.com>
2024-12-06 00:30:15 -0300

    [Agregamos] iconos de las tecnologías

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,46 +1,49 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import proyecto1 from '../../assets/proyectosImg/proyecto1.png';
-import proyecto2 from '../../assets/proyectosImg/imagenProyecto2.png';
-import proyecto3 from '../../assets/proyectosImg/imagenProyecto3.png';
-import { AcordionProject } from './AcordionProject';
-import { useState } from 'react';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import proyecto1 from "../../assets/proyectosImg/proyecto1.png";
+import proyecto2 from "../../assets/proyectosImg/imagenProyecto2.png";
+import proyecto3 from "../../assets/proyectosImg/imagenProyecto3.png";
+import { AcordionProject } from "./AcordionProject";
+import { useState } from "react";
 import "./project.css";
-import { Technologies } from './Technologies';
+import { Technologies } from "./Technologies";
+import github from "../../assets/ImgIconos/Github.svg"; // Importar el logo de GitHub
 
 export const Projects = () => {
-
   const [activeAccordions, setActiveAccordions] = useState([]); // Estado global de acordeones activos
-  
+
   const projects = [
     {
       id: 1,
-      title: 'Fashion Brand',
-      description: 'E-Commerce para una tienda de ropa, un proyecto que me ha permitido poner en práctica y reforzar habilidades clave en PowerApps, SharePoint, y PowerAutomate',
+      title: "Fashion Brand",
+      description:
+        "E-Commerce para una tienda de ropa, un proyecto que me ha permitido poner en práctica y reforzar habilidades clave en PowerApps, SharePoint, y PowerAutomate",
       image: proyecto1,
-      technologies: ['PowerApps', 'SharePoint', 'PowerAutomate']
+      technologies: ["PowerApp", "SharePoint", "PowerAutomate"],
     },
     {
       id: 2,
-      title: 'Dashboard Layout Project',
-      description: 'Este es un proyecto de Dashboard Layout creado con React, que proporciona una interfaz de usuario para administrar contenido en un panel de administración.',
+      title: "Dashboard Layout Project",
+      description:
+        "Este es un proyecto de Dashboard Layout creado con React, que proporciona una interfaz de usuario para administrar contenido en un panel de administración.",
       image: proyecto2,
-      technologies: ['React', 'Node.js', 'MongoDB']
+      technologies: ["React", "Node.js", "MongoDB"],
     },
     {
       id: 3,
-      title: 'Barbería Alura',
-      description: 'Este proyecto es un sitio web diseñado para mostrar los servicios y productos de una barbería ficticia, proporcionando una experiencia visual atractiva y moderna.',
+      title: "Barbería Alura",
+      description:
+        "Este proyecto es un sitio web diseñado para mostrar los servicios y productos de una barbería ficticia, proporcionando una experiencia visual atractiva y moderna.",
       image: proyecto3,
-      technologies: ['HTML', 'CSS', 'JavaScript']
+      technologies: ["HTML", "CSS", "JavaScript"],
     },
   ];
 
   const handleAccordionToggle = (id, isExpanded) => {
     if (isExpanded) {
-      setActiveAccordions(prev => [...prev, id]);
+      setActiveAccordions((prev) => [...prev, id]);
     } else {
-      setActiveAccordions(prev => prev.filter(activeId => activeId !== id));
+      setActiveAccordions((prev) => prev.filter((activeId) => activeId !== id));
     }
   };
 
@@ -49,41 +52,89 @@ export const Projects = () => {
       <div className="row">
         <div className="d-flex flex-wrap justify-content-center gap-4 p-4">
           {projects.map((project) => {
-            const isExpanded = activeAccordions.includes(`desc-${project.id}`) || activeAccordions.includes(`tech-${project.id}`);
+            const isExpanded =
+              activeAccordions.includes(`desc-${project.id}`) ||
+              activeAccordions.includes(`tech-${project.id}`);
             return (
-              <Card 
-                className={`translucent-card ${isExpanded ? 'expanded' : ''}`}
+              <Card
+                className={`translucent-card ${isExpanded ? "expanded" : ""}`}
                 style={{
-                  width: '18rem',
-                  height: isExpanded ? 'auto' : '32rem', // Altura dinámica
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
+                  width: "18rem",
+                  height: isExpanded ? "auto" : "35rem", // Altura dinámica
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                 }}
-                key={project.id}>
-                <Card.Img 
-                  className='imgCard' 
-                  variant="top" 
-                  src={project.image} />
-                <Card.Body>
+                key={project.id}
+              >
+                <Card.Img
+                  className="imgCard"
+                  variant="top"
+                  src={project.image}
+                />
+                <Card.Body
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
                   <Card.Title>{project.title}</Card.Title>
-                  <AcordionProject 
+                  <AcordionProject
                     id={`desc-${project.id}`}
                     activeAccordion={activeAccordions}
                     setActiveAccordion={handleAccordionToggle}
                     description={project.description}
                     nombreHeader="Más sobre el proyecto"
-                    onToggle={(isExpanded) => handleAccordionToggle(`desc-${project.id}`, isExpanded)}
+                    onToggle={(isExpanded) =>
+                      handleAccordionToggle(`desc-${project.id}`, isExpanded)
+                    }
                   />
-                  <AcordionProject 
+                  <AcordionProject
                     id={`tech-${project.id}`}
                     activeAccordion={activeAccordions}
                     setActiveAccordion={handleAccordionToggle}
-                    description={<Technologies technologies={project.technologies} />}
+                    description={
+                      <Technologies technologies={project.technologies} />
+                    }
                     nombreHeader="Tecnologías utilizadas"
-                    onToggle={(isExpanded) => handleAccordionToggle(`tech-${project.id}`, isExpanded)}
+                    onToggle={(isExpanded) =>
+                      handleAccordionToggle(`tech-${project.id}`, isExpanded)
+                    }
                   />
-                  <Button variant="primary">Ver más</Button>
+                  <Button
+                    variant="primary"
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      backgroundColor: "white", // Fondo blanco por defecto
+                      color: "black",
+                      border: "2px solid black", // Agrega un borde para definir el contorno
+                      borderRadius: "5px", // Bordes redondeados
+                      padding: "8px 16px", // Padding para ajustar el tamaño del botón
+                      transition: "background-color 0.3s ease", // Transición suave al cambiar el fondo
+                    }}
+                    onMouseEnter={(e) =>
+                      (e.target.style.backgroundColor = "cadetblue")
+                    } // Efecto hover: al pasar el mouse
+                    onMouseLeave={(e) =>
+                      (e.target.style.backgroundColor = "white")
+                    } // Restaura el color de fondo al dejar de pasar el mouse
+                  >
+                    <img
+                      src={github}
+                      alt="GitHub"
+                      style={{
+                        width: "35px",
+                        height: "35px",
+                        marginRight: "8px",
+                      }} // Ajusta el tamaño de la imagen
+                    />
+                    GitHub
+                  </Button>
                 </Card.Body>
               </Card>
             );
--- src/components/Projects/Technologies.jsx
+++ src/components/Projects/Technologies.jsx
@@ -1,23 +1,48 @@
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs } from 'react-icons/fa'; // Asegúrate de importar los iconos necesarios
+import sharepoint from '../../assets/ImgIconos/iconsSharepoint.png';
+import html from '../../assets/ImgIconos/Html.svg';
+import css from '../../assets/ImgIconos/Css.svg';
+import github from '../../assets/ImgIconos/Github.svg';
+import js from '../../assets/ImgIconos/Javascript.svg';
+import java from '../../assets/ImgIconos/Java.svg';
+import mongo from '../../assets/ImgIconos/Mongodb.svg';
+import mysql from '../../assets/ImgIconos/Mysql.svg';
+import react from '../../assets/ImgIconos/React.svg';
+import node from '../../assets/ImgIconos/Nodejs.svg';
+import powerapp from '../../assets/ImgIconos/iconPoweApp.png';
+import powerautomate from '../../assets/ImgIconos/iconsPowerAutomate.png';
 
 const technologyIcons = {
-    'HTML': <FaHtml5 />,
-    'CSS': <FaCss3Alt />,
-     'JavaScript': <FaJs />, 
-     'React': <FaReact />,
-      'Node.js': <FaNodeJs />,
-   };
-export const Technologies = ({ technologies }) => {
+'HTML': html,
+'SharePoint': sharepoint,
+'CSS': css,
+'GitHub': github,
+'JavaScript': js,
+'Java': java,
+'MongoDB': mongo,
+'Mysql': mysql,
+'PowerApp': powerapp,
+'PowerAutomate': powerautomate,
+'React': react,
+'Node.js': node,
+};
 
-    if (!technologies) {
-         console.error("Technologies prop is undefined");
-          return null; }
-  return (
-    <div className="technologies-list">
-         {technologies.map(tech => ( 
-            <div key={tech} className="technology-icon">
-                 {technologyIcons[tech] || tech} {/* Muestra el icono si existe, sino el nombre */} 
-                 </div> ))}
-             </div>
-  )
-}
+export const Technologies = ({ technologies }) => {
+if (!technologies || !Array.isArray(technologies)) {
+    console.error("Technologies prop is missing or not an array");
+    return null;
+    }
+
+    return (
+        <div className="technologies-list">
+            {technologies.map(tech => (
+                <div key={tech} className="technology-icon">
+                    {technologyIcons[tech] ? (
+                        <img src={technologyIcons[tech]} alt={tech} className="tech-icon" />
+                    ) : (
+                        tech // Si no hay imagen, muestra el nombre
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+};
